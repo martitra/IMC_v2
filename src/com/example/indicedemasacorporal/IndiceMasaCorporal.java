0585052b47cbc80b1032d5d19d3f30c7cdfaac69
GabinoProfesor <lag@fernandowirtz.com>
2014-10-06 13:32:52 +0200

    Primer commit

--- src/com/example/indicedemasacorporal/IndiceMasaCorporal.java
+++ src/com/example/indicedemasacorporal/IndiceMasaCorporal.java
@@ -1,54 +1,81 @@
 package com.example.indicedemasacorporal;
 
+import android.util.Log;
+
 public class IndiceMasaCorporal {
-	
+
 	/**
 	 * Recordatorio: Clases inmutables.
 	 */
-	private Integer altura;
 	private Double peso;
-	
-/**
+	private Integer altura;
+
+	/**
 	 * 
 	 */
 	public IndiceMasaCorporal() {
 		// TODO Auto-generated constructor stub
-		this.altura = new Integer(0);
 		this.peso = new Double(0.0);
+		this.altura = new Integer(0);
+
 	}
-/**
- * Constructor	
- * @param altura
- * @param peso
- */
-public IndiceMasaCorporal(Integer altura, Double peso) {
-		super();
+
+	/**
+	 * Constructor
+	 * 
+	 * @param peso
+	 * @param altura
+	 */
+	public IndiceMasaCorporal(Double peso, Integer altura) {
+		// super();
+		this.peso = peso;
 		this.altura = altura;
+
+	}
+
+	/**
+	 * @return the altura
+	 */
+	public Integer getAltura() {
+		return altura;
+	}
+
+	/**
+	 * @param altura
+	 *            the altura to set
+	 */
+	public void setAltura(Integer altura) {
+		this.altura = altura;
+	}
+
+	/**
+	 * @return the peso
+	 */
+	public Double getPeso() {
+		return peso;
+	}
+
+	/**
+	 * @param peso
+	 *            the peso to set
+	 */
+	public void setPeso(Double peso) {
 		this.peso = peso;
 	}
-/**
- * @return the altura
- */
-public Integer getAltura() {
-	return altura;
-}
-/**
- * @param altura the altura to set
- */
-public void setAltura(Integer altura) {
-	this.altura = altura;
-}
-/**
- * @return the peso
- */
-public Double getPeso() {
-	return peso;
-}
-/**
- * @param peso the peso to set
- */
-public void setPeso(Double peso) {
-	this.peso = peso;
-}
+
+	public Double valorIndiceMasaCorporal() throws Exception{
+
+			if (altura.doubleValue()==0.0) throw new Exception("Div by Zero");
+			return peso.doubleValue() / Math.pow(altura.doubleValue(), 2.0);
+
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "IndiceMasaCorporal [peso=" + peso + ", altura=" + altura + "]";
+	}
 
 }
--- src/com/example/indicedemasacorporal/MainActivity.java
+++ src/com/example/indicedemasacorporal/MainActivity.java
@@ -2,43 +2,62 @@ package com.example.indicedemasacorporal;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	private IndiceMasaCorporal imc;
+	EditText editPeso;
+	EditText editAltura;
 
-    /**
-     * onButtonClickCalcularIMC
-     */
-    public void onButtonClickCalcularIMC(View v){
-    	
-    }
-    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		// Establecer identificadores
+		editPeso = (EditText) findViewById(R.id.editPeso);
+		editAltura = (EditText) findViewById(R.id.editAltura);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	}
+
+	/**
+	 * onButtonClickCalcularIMC
+	 */
+	public void onButtonClickCalcularIMC(View v) {
+
+		try {
+			Double peso = Double.valueOf(editPeso.getText().toString());
+			Integer altura = Integer.valueOf(editAltura.getText().toString());
+
+			imc = new IndiceMasaCorporal(peso, altura);
+			Log.d("IMC", imc.toString());
+			Log.d("IMC", String.valueOf(imc.valorIndiceMasaCorporal()));
+		} catch (Exception e) {
+			Log.e("IMC_ERROR", e.getMessage());
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
